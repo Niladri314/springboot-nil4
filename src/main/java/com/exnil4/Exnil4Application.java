bdcf301001671853ba37a19cddbc2ca0edf16c64
Niladri314 <126091593+Niladri314@users.noreply.github.com>
2025-09-24 21:36:59 +0530

    Update Exnil4Application.java

--- src/main/java/com/exnil4/Exnil4Application.java
+++ src/main/java/com/exnil4/Exnil4Application.java
@@ -8,12 +8,8 @@ public class Exnil4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Exnil4Application.class, args);
-		System.out.println("hot fix done");
-<<<<<<< HEAD
-=======
 		System.out.println("login development");
-		System.out.println("sign-up done");
->>>>>>> 7f494361f2c608b06f21aba4c48f611bf20833a2
+	
 	}
 
 }
